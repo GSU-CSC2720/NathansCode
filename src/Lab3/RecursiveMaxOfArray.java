1140f614669b9022defa2821b137c6b894f83e53
Nathan Larsen <nlarsen@nathan2055.com>
2019-09-09 15:37:26 -0500

    Initial Lab 3 files

---
+++ src/Lab3/RecursiveMaxOfArray.java
@@ -0,0 +1,34 @@
+package Lab3;
+
+/**
+ * Use a double recursion to compute the maximum of an array of values.
+ * 
+
+ */
+public class RecursiveMaxOfArray
+{
+
+    
+    /**
+     * Compute the maximum of a range of values in an array recursively.
+     *
+     * @param data   An array of integers.
+     * @param from  The low index for searching for the maximum.
+     * @param to    The high index for searching for the maximum.
+     * 
+     * preconditions: from <= to, from >=0, to<length, length>0
+     *                
+     * @return     The maximum value in the array.
+     */
+    
+    public  int max(int data[], int from, int to)
+    {
+        int result = 0;
+        
+        // ADD YOUR CODE HERE
+
+        return result;
+    }
+    
+    
+}
