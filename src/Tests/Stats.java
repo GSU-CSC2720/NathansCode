cd917fbf0f346030c39aae4586c33339f48002bc
Nathan Larsen <nlarsen@nathan2055.com>
2019-09-17 10:43:12 -0400

    Initial version of tests for lab 4 pre-lab

---
+++ src/Tests/Stats.java
@@ -0,0 +1,18 @@
+package Tests;
+
+public class Stats {
+    private int min = 0, max = 0, avg = 0;
+
+    public void giveValue(int value) {
+        if (value < min) {
+            min = value;
+        } else if (value > max) {
+            max = value;
+        }
+        avg = (avg + value) / 2;
+    }
+
+    public void printValues() {
+        System.out.println("Minimum: " + min + ", maximum: " + max + ", average: " + avg);
+    }
+}
