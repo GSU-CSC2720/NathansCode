267554b349a5bc93d424500d3f9f5e3acf6cccde
Nathan Larsen <nlarsen@nathan2055.com>
2019-09-17 10:28:36 -0400

    Optimize and clean initial lab 4 code

--- src/Lab4/SortArray.java
+++ src/Lab4/SortArray.java
@@ -1,3 +1,4 @@
+package Lab4;
 
 /********************************************************************
  * Class for sorting an array of Comparable objects from smallest to
@@ -9,12 +10,14 @@ public class SortArray {
     /**************************************************************
      * ITERATIVE SELECTION SORT
      **************************************************************/
-    
-    /** Sorts the first n objects in an array into ascending order.
+
+    /**
+     * Sorts the first n objects in an array into ascending order.
+     *
      * @param a An array of Comparable objects.
      * @param n An integer > 0.
      */
-    public static <T extends Comparable<? super T>> 
+    public static <T extends Comparable<? super T>>
     void selectionSort(T[] a, int n) {
         for (int index = 0; index < n - 1; index++) {
             int indexOfNextSmallest = getIndexOfSmallest(a, index, n - 1);
@@ -23,16 +26,18 @@ public class SortArray {
         } // end for
     } // end selectionSort
 
-    /** Finds the index of the smallest value in an array a.
-     * @param a An array of Comparable objects.
+    /**
+     * Finds the index of the smallest value in an array a.
+     *
+     * @param a     An array of Comparable objects.
      * @param first An integer >= 0 and < a.length that is the index of the first
-     * array entry to consider.
-     * @param last An integer >= 0 and < a.length that is the index of the last
-     * array entry to consider.
+     *              array entry to consider.
+     * @param last  An integer >= 0 and < a.length that is the index of the last
+     *              array entry to consider.
      * @return The index of the smallest value among
      * a[first], a[first+1], . . . , a[last].
      */
-    public static <T extends Comparable<? super T>> 
+    public static <T extends Comparable<? super T>>
     int getIndexOfSmallest(T[] a, int first, int last) {
         T min = a[first];
         int indexOfMin = first;
@@ -46,14 +51,16 @@ public class SortArray {
         return indexOfMin;
     } // end getIndexOfSmallest
 
-    /** Swaps the array entries a[i] and a[j].
+    /**
+     * Swaps the array entries a[i] and a[j].
+     *
      * @param a An array of  objects.
      * @param i An integer >= 0 and < a.length.
-     * @param j An integer >= 0 and < a.length. 
-     * 
-     * Modified from Carrano to use generics.
+     * @param j An integer >= 0 and < a.length.
+     *          <p>
+     *          Modified from Carrano to use generics.
      */
-    private static <T> 
+    private static <T>
     void swap(T[] a, int i, int j) {
         T temp = a[i];
         a[i] = a[j];
