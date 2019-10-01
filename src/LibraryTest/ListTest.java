e7102e77b94bab4c60804de351556822dcb32280
Nathan Larsen <nlarsen@nathan2055.com>
2019-10-01 11:56:39 -0400

    Experimentation with IntelliJ's module importation system

---
+++ src/LibraryTest/ListTest.java
@@ -0,0 +1,9 @@
+package LibraryTest;
+
+import NathanList.NathanListImplementation;
+
+public class ListTest {
+    public static void main(String[] args) {
+        NathanListImplementation test = new NathanListImplementation();
+    }
+}
