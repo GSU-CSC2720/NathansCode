3ff3c8eb16b6240858f76bd8b7537ff762740d1d
Nathan Larsen <nlarsen@nathan2055.com>
2019-10-08 10:45:34 -0400

    Implement displayStack()

--- src/com/nathan2055/lab60Stack/DriverStack.java
+++ src/com/nathan2055/lab60Stack/DriverStack.java
@@ -1,5 +1,10 @@
 package com.nathan2055.lab60Stack;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 /**
  * ADT STACK HAS 3 IMPLEMENTATION
  * <p>
@@ -15,7 +20,33 @@ public class DriverStack {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        displayStack(getRandomStack(10));
     }
 
+    public static void displayStack(StackInterface stack) {
+        String items = "";
+        List temp = new ArrayList();
+        int size = 0;
+        for (int i = 0; !stack.isEmpty(); i++) {
+            temp.add(stack.pop());
+            size++;
+            items = items + temp.get(i);
+            if (!stack.isEmpty()) {
+                items = items + ", ";
+            }
+        }
+        for (int i = 0; i < size; i++) {
+            stack.push(temp.get(i));
+        }
+        System.out.println(items);
+    }
+
+    public static StackInterface getRandomStack(int size) {
+        Random rand = new Random();
+        StackInterface stack = new StackReferenceBased();
+        for (int i = 0; i < size; i++) {
+            stack.push(rand.nextInt());
+        }
+        return stack;
+    }
 }
